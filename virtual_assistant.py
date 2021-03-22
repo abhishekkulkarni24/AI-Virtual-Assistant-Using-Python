6264ee42f0e5b79e70b3365d9c77ad03aa326350
Abhishek Kulkarni <kulkarni.abhishek2407@gmail.com>
2021-03-22 19:40:41 +0530

    Wish feature added

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -6,11 +6,20 @@ import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[1].id)
+# print(voices[1].id)
 engine.setProperty('voice',voices[1].id)
 
 def assistantVoice(audio):
     engine.say(audio)
     engine.runAndWait()
 
-assistantVoice("Hello Boss")
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        assistantVoice("Good Morning Sir")
+    elif hour>=12 and hour<18:
+        assistantVoice("Good Afternoon Sir") 
+    else:
+        assistantVoice("Good Evening Sir")   
+
+wish()        
