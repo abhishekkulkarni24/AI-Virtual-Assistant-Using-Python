ecd6586d498e43f094a11af29b01f8a78556f91f
Abhishek Kulkarni <kulkarni.abhishek2407@gmail.com>
2021-03-23 14:53:24 +0530

    Speech recognition working code updated

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -22,26 +22,50 @@ def wish():
     else:
         assistantVoice("Good Evening Sir")   
 
-def takeCommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening....")
-        r.pause_threshold=1
-        audio=r.listen(source)    
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+           successful
+    "error":   `None` if no error occured, otherwise a string containing
+           an error message if the API could not be reached or
+           speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+           otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        print("Listening ...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    
     try:
-        print("Recognizing....")
-        query = r.recognize_google(audio,language='en-in')
-        print("user said " + query)
-    except Exception as e:
-        print(e)
-        print("Sorry.. Say that again please")   
-        return "None"
-    return query   
+        res =    recognizer.recognize_google(audio)
+        print(res)
+    except sr.RequestError:
+        print("error")
+
+    return res
 
 wish()
 status = True
+
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+
+
 while status:
-    query = takeCommand().lower()
+    query = recognize_speech_from_mic(recognizer,microphone).lower()
     if "what is" in query or "who is" in query:
         assistantVoice("searching in wekipedia")
         query = query.replace("wikipedia","")
